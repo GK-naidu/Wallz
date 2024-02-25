b8115e3b98dab868055e0002e27969a14a33fdcb
G.K.Naidu <g.k.naidu@icloud.com>
2024-02-25 17:53:13 +0530

    the tab bar is added , favourite list update  is still under progress.

---
+++ wallz/FavouriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteView.swift
+//  wallz
+//
+//  Created by G.K.Naidu on 25/02/24.
+//
+
+import SwiftUI
+
+struct FavouriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteView()
+}
