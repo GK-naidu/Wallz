01f51c750b295183f3c4c2b529efee126ea04edb
G.K.Naidu <g.k.naidu@icloud.com>
2024-04-02 19:59:20 +0530

    categoriesView() , the image and text are beign loaded from the url ( only image ) text - is local , navigationLink needs to be added

---
+++ wallz/Categories.swift
@@ -0,0 +1,18 @@
+//
+//  Categories.swift
+//  wallz
+//
+//  Created by G.K.Naidu on 01/04/24.
+//
+
+import SwiftUI
+
+struct Categories: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Categories()
+}
---
+++ wallz/categoryView.swift
@@ -0,0 +1,18 @@
+//
+//  categoryView.swift
+//  wallz
+//
+//  Created by G.K.Naidu on 02/04/24.
+//
+
+import SwiftUI
+
+struct categoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    categoryView()
+}
