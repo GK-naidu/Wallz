b3fbb42231f6e6a860dc08ce43879b4475892f10
G.K.Naidu <g.k.naidu@icloud.com>
2024-04-06 00:49:29 +0530

    categories are being loaded , but on tap doesn't navigate

---
+++ wallz/CategoryWall.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryWall.swift
+//  wallz
+//
+//  Created by G.K.Naidu on 05/04/24.
+//
+
+import SwiftUI
+
+struct CategoryWall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryWall()
+}
